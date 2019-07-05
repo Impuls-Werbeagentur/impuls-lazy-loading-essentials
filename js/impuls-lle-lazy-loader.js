ff55f4f8d6893b26e24390564076269609ef9701
Impuls Werbeagentur <46940656+impuls-wa@users.noreply.github.com>
2019-07-05 10:51:43 +0200

    Add GMaps Lazy Loading Capability
    Using the Class "impuls-lle-lazy-gmap" and the data-keys data-gmapsapikey and data-gmapscallback, Google Maps can be lazy loaded now.

--- js/impuls-lle-lazy-loader.js
+++ js/impuls-lle-lazy-loader.js
@@ -27,9 +27,30 @@ let lazyObjectObserver = new IntersectionObserver(function(entries, observer) {
 		});
 	},{ rootMargin: "0px 0px 0px 0px" });
 
+let lazyGMapsObserver = new IntersectionObserver(function(entries, observer) {
+	entries.forEach(function(entry) {
+		if (entry.isIntersecting) {
+			let lazyGMapContainer = entry.target;
+			apikey = lazyGMapContainer.dataset.lazygmapsapikey;
+			callback = lazyGMapContainer.dataset.lazygmapscallback;
+			var script = document.createElement('script');
+			script.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key='+apikey+'&callback='+callback);
+			document.head.appendChild(script);
+			lazyGMapsObserver.unobserve(lazyGMapContainer);
+		}
+	});
+},{
+	rootMargin: "0px 0px 0px 0px"
+});
+
+
 document.addEventListener("DOMContentLoaded", function() {
   var lazyObjects = [].slice.call(document.querySelectorAll(".impuls-lle-lazy"));
   lazyObjects.forEach(function(lazyObject) {
 		lazyObjectObserver.observe(lazyObject);
+  });
+	var lazyGMapsObjects = [].slice.call(document.querySelectorAll(".impuls-lle-lazy-gmap"));
+  lazyGMapsObjects.forEach(function(lazyGMapsObject) {
+		lazyGMapsObserver.observe(lazyGMapsObject);
   });
 });
